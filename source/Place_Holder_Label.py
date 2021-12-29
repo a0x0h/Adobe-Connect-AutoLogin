9e746e1c86195df6951efa2b4c4ab3730a79dbd0
Prx001 <munichbayern2005@gmail.com>
2021-12-29 22:57:51 +0330

    Upgrade

--- source/Place_Holder_Label.py
+++ source/Place_Holder_Label.py
@@ -1,18 +1,16 @@
-from PyQt5.QtCore import pyqtSignal, pyqtSlot, pyqtProperty
+from PyQt5.QtCore import Qt, pyqtSignal, pyqtSlot, pyqtProperty
 from PyQt5.QtWidgets import QLabel
 
 
 class PlaceHolderLabel(QLabel):
-	clicked = pyqtSignal()
-
 	def __init__(self, text="", parent=None):
 		if parent is not None:
 			super().__init__(text, parent=parent)
 		elif parent is None:
 			super().__init__(text)
+		self.setAttribute(Qt.WA_TransparentForMouseEvents)
 		self.new_font = self.font()
 		self._point_size = self.font().pointSize()
-		self.click = False
 
 	def setFont(self, font):
 		super().setFont(font)
@@ -29,13 +27,3 @@ class PlaceHolderLabel(QLabel):
 		self.resize(self.sizeHint())
 
 	point_size = pyqtProperty(int, get_point_size, set_point_size)
-
-	def mousePressEvent(self, event):
-		self.click = True
-		return super().mousePressEvent(event)
-
-	def mouseReleaseEvent(self, event):
-		if self.click:
-			self.clicked.emit()
-			self.click = False
-		return super().mouseReleaseEvent(event)
